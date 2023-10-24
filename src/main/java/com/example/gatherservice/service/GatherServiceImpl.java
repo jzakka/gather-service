d417f6215054b6708c1fcfc4cb448692f507f2e1
Jzakka <mouse4786@gmail.com>
2023-10-24 11:54:03 +0900

    feat: 모임 생성 검증

--- src/main/java/com/example/gatherservice/service/GatherServiceImpl.java
+++ src/main/java/com/example/gatherservice/service/GatherServiceImpl.java
@@ -7,21 +7,30 @@ import com.example.gatherservice.repository.GatherRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
-public class GatherServiceImpl implements GatherService{
+public class GatherServiceImpl implements GatherService {
     private final GatherRepository gatherRepository;
     private final ModelMapper mapper;
+    private final Environment env;
 
     @Override
     public GatherDto createGather(GatherDto gatherDto) {
+        validate(gatherDto);
+
         gatherDto.setGatherId(UUID.randomUUID().toString());
         GatherEntity gather = mapper.map(gatherDto, GatherEntity.class);
 
@@ -29,6 +38,37 @@ public class GatherServiceImpl implements GatherService{
 
         GatherDto savedGatherDto = mapper.map(savedGather, GatherDto.class);
         return savedGatherDto;
+    }
+
+    private void validate(GatherDto gatherDto) {
+        /**
+         * 모임 검증
+         *
+         * 모임 시작날짜가 끝날짜보다 앞서야함
+         * 모임 시작시간이 끝시간보다 앞서야함 (정오를 걸치는 모임을 생성할 수 없음)
+         * 모임 시간이 시작시간 ~ 끝시간보다 작아야함
+         * 모임 마감일자가 모임 시간시간보다 하루 이상
+         */
+        String errorMessage = null;
+        LocalDateTime startDateTime = LocalDateTime.of(gatherDto.getStartDate(), gatherDto.getStartTime());
+        long hoursToMillis = gatherDto.getDuration().getHour() * 60 * 60 * 1000;
+        long minutesToMillis = gatherDto.getDuration().getMinute() * 60 * 1000;
+        long secondsToMillis = gatherDto.getDuration().getSecond() * 1000;
+        long durationTimeStamp = hoursToMillis + minutesToMillis + secondsToMillis;
+
+        if (gatherDto.getStartDate().isAfter(gatherDto.getEndDate())) {
+            errorMessage = env.getProperty("gather.validation.date-invalid-msg");
+        } else if (gatherDto.getStartTime().isAfter(gatherDto.getEndTime())) {
+            errorMessage = env.getProperty("gather.validation.time-invalid-msg");
+        } else if (durationTimeStamp > Duration.between(gatherDto.getStartTime(), gatherDto.getEndTime()).toMillis()) {
+            errorMessage = env.getProperty("gather.validation.duration-invalid-msg");
+        } else if (!gatherDto.getDeadLine().plusDays(1).isBefore(startDateTime)) {
+            errorMessage = env.getProperty("gather.validation.deadline-invalid-msg");
+        }
+
+        if (errorMessage != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+        }
     }
 
     @Override
--- src/test/java/com/example/gatherservice/service/GatherServiceImplTest.java
+++ src/test/java/com/example/gatherservice/service/GatherServiceImplTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,13 +19,16 @@ class GatherServiceImplTest {
     @Autowired
     GatherService gatherService;
 
+    @Autowired
+    Environment env;
+
     @Test
     @DisplayName("모임 생성 테스트 -> 성공")
     void createGatherSuccess() {
         GatherDto dto = dummyGatherDto(
                 "테스트 모임", "테스트 설명",
-                LocalDate.now(), LocalDate.now().plusDays(1),
-                LocalTime.now(), LocalTime.now().plusHours(1),
+                LocalDate.now().plusDays(10), LocalDate.now().plusDays(11),
+                LocalTime.now(), LocalTime.now().plusHours(3),
                 LocalTime.of(1, 30),
                 LocalDateTime.now().plusDays(7)
         );
@@ -31,6 +36,70 @@ class GatherServiceImplTest {
         GatherDto result = gatherService.createGather(dto);
 
         assertThat(result.getName()).isEqualTo("테스트 모임");
+    }
+
+    @Test
+    @DisplayName("잘못된 날짜 테스트")
+    void createInvalidDateGather() {
+        GatherDto dto = dummyGatherDto(
+                "테스트 모임", "테스트 설명",
+                LocalDate.now().plusDays(1), LocalDate.now(),
+                LocalTime.now(), LocalTime.now().plusHours(1),
+                LocalTime.of(1, 30),
+                LocalDateTime.now().plusDays(7)
+        );
+
+        assertThatThrownBy(() -> gatherService.createGather(dto))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining(env.getProperty("gather.validation.date-invalid-msg"));
+    }
+
+    @Test
+    @DisplayName("잘못된 시간 테스트")
+    void createInvalidTimeGather() {
+        GatherDto dto = dummyGatherDto(
+                "테스트 모임", "테스트 설명",
+                LocalDate.now(), LocalDate.now().plusDays(1),
+                LocalTime.now().plusHours(1), LocalTime.now(),
+                LocalTime.of(1, 30),
+                LocalDateTime.now().plusDays(7)
+        );
+
+        assertThatThrownBy(() -> gatherService.createGather(dto))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining(env.getProperty("gather.validation.time-invalid-msg"));
+    }
+
+    @Test
+    @DisplayName("잘못된 모임기간 테스트")
+    void createInvalidDurationGather() {
+        GatherDto dto = dummyGatherDto(
+                "테스트 모임", "테스트 설명",
+                LocalDate.now().plusDays(9), LocalDate.now().plusDays(9),
+                LocalTime.now(), LocalTime.now().plusHours(1),
+                LocalTime.of(1, 30),
+                LocalDateTime.now().plusDays(7)
+        );
+
+        assertThatThrownBy(() -> gatherService.createGather(dto))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining(env.getProperty("gather.validation.duration-invalid-msg"));
+    }
+
+    @Test
+    @DisplayName("잘못된 마감기간 테스트")
+    void createInvalidDeadLineGather() {
+        GatherDto dto = dummyGatherDto(
+                "테스트 모임", "테스트 설명",
+                LocalDate.now().plusDays(9), LocalDate.now().plusDays(10),
+                LocalTime.now(), LocalTime.now().plusHours(2),
+                LocalTime.of(1, 30),
+                LocalDateTime.now().plusDays(9)
+        );
+
+        assertThatThrownBy(() -> gatherService.createGather(dto))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining(env.getProperty("gather.validation.deadline-invalid-msg"));
     }
 
     private GatherDto dummyGatherDto(String name,
